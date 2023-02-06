f121b467f1ee5408d805eba67fb8bb62af4245b3
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-02-06 13:04:10 +0100

    slideshow then

--- slideshow-demo.rkt
+++ slideshow-demo.rkt
@@ -1,40 +1,25 @@
 #lang leo
 
-do require leo/slideshow
+require leo/slideshow
 
-do slide:
+then slide:
+  magenta filled-rounded-rectangle: 800 8
   big big big text "Leonardo"
-  big text "This is my awesome slideshow!!!"
-  item "Share with your friends at the bar and the library."
-  item "Don't forget to like and subscribe."
-  item:
-    "What"
-    bold "else"
-    "could I ask you for?"
+  big blue text "Welcome to my world!!!"
+  magenta filled-rounded-rectangle: 800 8
 
-do slide:
-  big big big text "Now..."
-  big text "What do you want me to do?"
+then slide:
+  big big text "Unfortunately..."
+  text "There's really nothing more I can show to you now."
 
-do slide:
-  big big big text "This is my last slide"
-  paragraph "Are you satisfied with what you saw?"
-  item "Yes"
-  item "No"
+then slide:
+  big big big text "So..."
 
+then slide:
+  big big text "Remember to"
+  item "Share it with your friends at the bar and the library,"
+  item "Don't forget to like and subscribe,"
+  item "...and take care, my friend."
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+then slide:
+  big big big text "The end"
--- slideshow.rkt
+++ slideshow.rkt
@@ -3,10 +3,16 @@
 do require:
   slideshow/widescreen/base
   slideshow/text
+  slideshow/pict
+  slideshow/code-pict
+  pict/color
 
 do provide all-from-out:
   slideshow/widescreen/base
   slideshow/text
+  slideshow/pict
+  slideshow/code-pict
+  pict/color
 
 do define: text t
 do define: bold bt
