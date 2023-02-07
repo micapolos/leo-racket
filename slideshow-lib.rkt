a5770f9b3517191219a81d16310b76834caa0fdf
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-02-07 22:44:09 +0100

    jack-o-lantern

---
+++ slideshow-lib.rkt
@@ -0,0 +1,21 @@
+#lang leo
+
+require:
+  slideshow/widescreen/base
+  slideshow/text
+  slideshow/pict
+  slideshow/code-pict
+  pict/color
+
+provide all-from-out:
+  slideshow/widescreen/base
+  slideshow/text
+  slideshow/pict
+  slideshow/code-pict
+  pict/color
+
+variable text t
+
+variable bold bt
+
+variable paragraph para
