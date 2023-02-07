78c371d5a8cdb4f72dfdb129d73ccb70fd92c69d
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-02-07 22:40:38 +0100

    the -> do

--- gui.rkt
+++ gui.rkt
@@ -23,33 +23,39 @@ variable canvas new:
     for: canvas dc
     doing
       dc
-      invoke-preserving-transformation function doing:
+      invoke-preserving-transformation function doing
         variable time
-          current-inexact-monotonic-milliseconds:
+          current-inexact-milliseconds:
           divided-by 1000.0
+
         variable half-width
           send: canvas get-width
           divided-by 2
+
         variable half-height
           send: canvas get-height
           divided-by 2
+
         variable rotation
           time
           plus 0.5
           times 7.5
           sin
           times 0.5
+
         variable scale
           time
           times 15
           sin
           plus 2.5
-        send: dc translate half-width half-height
-        send: dc rotate rotation
-        send: dc scale scale scale
-        send: dc translate -50 -50
-        draw-pict: pict dc 0 0
-        refresh:
+
+        do:
+          send: dc translate half-width half-height
+          send: dc rotate rotation
+          send: dc scale scale scale
+          send: dc translate -50 -50
+          draw-pict: pict dc 0 0
+          refresh:
 
 refresh:
 does
@@ -57,7 +63,6 @@ does
     timer%
     interval interval
     just-once? true
-    notify-callback function doing
-      send: canvas refresh
+    notify-callback function doing send: canvas refresh
 
 send: frame show true
