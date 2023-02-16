3b1c9be66583c08323897231494b283701ab32d1
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-02-16 14:39:02 +0100

    type-select

--- typed/type-select.rkt
+++ typed/type-select.rkt
@@ -132,3 +132,42 @@
     (list number-type (symbol-type `foo) string-type)
     (symbol-type `boolean))
   #f)
+
+; ------------------------------------------------------------------
+
+(define (struct-type-body-get-indexed 
+    ($struct-type-body : StructTypeBody)
+    ($selector : Type))
+  (type-list-selector-indexed 
+    (struct-type-body-type-list $struct-type-body) 
+    $selector))
+
+(define (type-body-get-indexed 
+    ($type-body : TypeBody)
+    ($selector : Type))
+  (cond
+    ((struct-type-body? $type-body) 
+      (struct-type-body-get-indexed $type-body $selector))
+    ((choice-type-body? $type-body) #f)))
+
+(define (field-type-get-indexed 
+    ($field-type : FieldType)
+    ($selector : Type))
+  (type-body-get-indexed (field-type-body $field-type) $selector))
+
+(define (type-get-indexed 
+    ($type : Type)
+    ($selector : Type))
+  (cond
+    ((native-type? $type) #f)
+    ((symbol-type? $type) #f)
+    ((field-type? $type) (field-type-get-indexed $type $selector))
+    ((arrow-type? $type) #f)))
+
+(check-equal?
+  (type-get-indexed
+    (field-type `foo
+      (struct-type-body 
+        (list number-type (symbol-type `foo) string-type)))
+    string-type)
+  (cons 1 string-type))
