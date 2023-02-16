ee4128ef73f5c67c33fe1164e31083084333ca78
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-02-16 11:44:23 +0100

    syntax-as

--- typed/syntax-type.rkt
+++ typed/syntax-type.rkt
@@ -4,13 +4,27 @@
 
 (require 
   leo/typed/option
+  leo/typed/type-match
   leo/typed/type)
 
 (define (syntax-with-type ($syntax : Syntax) ($type : Type)) : Syntax
   (syntax-property $syntax `type $type))
 
-(define (syntax-type ($syntax : Syntax)) : Type
+(define (syntax-type-option ($syntax : Syntax)) : (Option Type)
   (define $value (syntax-property $syntax `type))
-  (cond
-    ((equal? $value #f) (error (format "Not typed ~s" $syntax)))
-    (else (cast $value Type))))
+  (and $value (cast $value Type)))
+
+(define (syntax-type ($syntax : Syntax)) : Type
+  (or
+    (syntax-type-option $syntax)
+    (error (format "Not typed ~s" $syntax))))
+
+(define (syntax-as ($syntax : Syntax) ($type : Type)) : Syntax
+  (define $syntax-type-option (syntax-type-option $syntax))
+  (if 
+    (and 
+      $syntax-type-option
+      (not (type-matches? $syntax-type-option $type)))
+    (error (format "(syntax-as ~a ~a)" $syntax $type))
+    (void))
+  (syntax-with-type $syntax $type))
---
+++ typed/type-match.rkt
@@ -0,0 +1,20 @@
+#lang typed/racket/base
+
+(provide (all-defined-out))
+
+(require 
+  leo/typed/type 
+  leo/testing)
+
+(define (type-matches? (actual : Type) (expected : Type)) : Boolean
+  (equal? actual expected))
+
+(define (type-match (actual : Type) (expected : Type))
+  (if (type-matches? actual expected) 
+    (void)
+    (error (format "Type mismatch, actual: ~a, expected: ~a" actual expected))))
+
+(define (type-matching (actual : Type) (expected : Type)) : Type
+  (if (type-matches? actual expected) 
+    actual
+    (error (format "Type mismatch, actual: ~a, expected: ~a" actual expected))))
