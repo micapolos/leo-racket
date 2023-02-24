31fa05dd3f990f74e98d993eab3e8890675ca4f0
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-02-24 13:37:18 +0100

    type-decompile

---
+++ typed/type-decompile.rkt
@@ -0,0 +1,42 @@
+#lang typed/racket/base
+
+(provide (all-defined-out))
+
+(require
+  leo/typed/type
+  leo/typed/types
+  leo/typed/testing)
+
+(define (type-decompile ($type : Type)) : Any
+  (cond
+    ((racket? $type) 
+      (racket-any $type))
+    ((symbol? $type) $type)
+    ((giving? $type) 
+      `(giving 
+        ,@(map type-decompile (giving-lhs-types $type))
+        ,@(map type-decompile (giving-rhs-types $type))))
+    ((list? $type) (map type-decompile $type))
+    ((any? $type)
+      `(any ,(type-decompile (any-type $type))))
+    ((thing? $type) `thing)
+    (else $type)))
+
+(check-equal? (type-decompile string-type) `string)
+(check-equal? (type-decompile number-type) `number)
+(check-equal? (type-decompile fixnum-type) `fixnum)
+(check-equal? (type-decompile flonum-type) `flonum)
+(check-equal? (type-decompile boolean-type) `boolean)
+(check-equal? (type-decompile (racket `foo)) `foo)
+(check-equal? (type-decompile `foo) `foo)
+(check-equal? (type-decompile (thing)) `thing)
+
+(check-equal? 
+  (type-decompile `(foo ,number-type ,string-type))
+  `(foo number string))
+
+(check-equal? 
+  (type-decompile (giving (list number-type string-type) (list boolean-type)))
+  `(giving number string boolean))
+
+(check-equal? (type-decompile (any number-type)) `(any number))
