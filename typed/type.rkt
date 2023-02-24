4896d5f0fd2f19036a42d12d5c9a45fc585084d9
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-02-24 10:04:17 +0100

    type refactor

--- typed/type.rkt
+++ typed/type.rkt
@@ -7,21 +7,25 @@
 ; TODO: Refactor to (struct type ((any : Any)))
 (define-type Type Any)
 
+; everything matches this type
+(struct thing ()
+  #:transparent
+  #:type-name Thing)
+
+; native racket type
 (struct racket ((any : Any))
   #:type-name Racket
   #:transparent)
 
+; function type
 (struct giving ((lhs-types : (Listof Type)) (rhs-types : (Listof Type)))
   #:transparent
   #:type-name Arrow)
 
+; type of types
 (struct any ((type : Type))
   #:transparent
   #:type-name TypeAny)
-
-(struct thing ()
-  #:transparent
-  #:type-name Thing)
 
 (define (type-arrow ($type : Type)) : Arrow
   (cond
