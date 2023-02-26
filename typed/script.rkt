7c9ddaf6144d7b8be5d8f3e4963ecd41ea87db7f
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-02-26 16:50:57 +0100

    syntax-script

--- typed/script.rkt
+++ typed/script.rkt
@@ -2,10 +2,7 @@
 
 (provide (all-defined-out))
 
-(require
-  leo/typed/base
-  leo/typed/testing
-  (for-syntax racket/base))
+(require leo/typed/base)
 
 (data script (line-list : (Listof Line)))
 
@@ -14,22 +11,3 @@
 (data field (symbol : Symbol) (script : Script))
 
 (data native (any : Any))
-
-(define (syntax-script ($syntax-list : (Listof Syntax))) : Script
-  (script (map syntax-line $syntax-list)))
-
-(define (syntax-line ($syntax : Syntax)) : Line
-  (let (($any (syntax-e $syntax)))
-    (cond
-      ((and 
-        (list? $any)
-        (not (null? $any))
-        (symbol? (syntax-e (car $any))))
-        (field (syntax-e (car $any)) (syntax-script (cdr $any))))
-      (else (native (syntax->datum $syntax))))))
-   
-(check-equal? (syntax-line #`1) (native 1))
-(check-equal? (syntax-line #`"foo") (native "foo"))
-(check-equal? (syntax-line #`()) (native `()))
-(check-equal? (syntax-line #`(foo)) (field `foo (script null)))
-(check-equal? (syntax-line #`(foo 1 "foo")) (field `foo (script (list (native 1) (native "foo")))))
---
+++ typed/syntax-script.rkt
@@ -0,0 +1,26 @@
+#lang typed/racket/base
+
+(provide (all-defined-out))
+
+(require
+  leo/typed/script
+  leo/typed/testing)
+
+(define (syntax-script ($syntax-list : (Listof Syntax))) : Script
+  (script (map syntax-line $syntax-list)))
+
+(define (syntax-line ($syntax : Syntax)) : Line
+  (let (($any (syntax-e $syntax)))
+    (cond
+      ((and 
+        (list? $any)
+        (not (null? $any))
+        (symbol? (syntax-e (car $any))))
+        (field (syntax-e (car $any)) (syntax-script (cdr $any))))
+      (else (native (syntax->datum $syntax))))))
+   
+(check-equal? (syntax-line #`1) (native 1))
+(check-equal? (syntax-line #`"foo") (native "foo"))
+(check-equal? (syntax-line #`()) (native `()))
+(check-equal? (syntax-line #`(foo)) (field `foo (script null)))
+(check-equal? (syntax-line #`(foo 1 "foo")) (field `foo (script (list (native 1) (native "foo")))))
