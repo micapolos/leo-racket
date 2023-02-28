97ca738fcd52c74e0b0429ef724f8e4b41f95728
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-02-28 20:37:17 +0100

    generate-temporary on type-symbol

--- compiler/generate-temporary.rkt
+++ compiler/generate-temporary.rkt
@@ -6,13 +6,21 @@
   racket/string
   leo/typed/stack
   leo/typed/testing
-  leo/compiler/racket
   leo/compiler/type
+  leo/compiler/type-utils
   leo/compiler/type-symbol
   leo/compiler/typed)
 
 (define (type-generate-temporary ($type : Type)) : Identifier
   (car (generate-temporaries (list (type-symbol $type)))))
+
+(check-equal?
+  (string-prefix?
+    (symbol->string (syntax-e (type-generate-temporary type-a)))
+    (symbol->string (type-symbol type-a)))
+  #t)
+
+; -------------------------------------------------------------------------
 
 (define (typed-syntax-generate-temporary ($typed : (Typed Syntax Type))) : Identifier
   (type-generate-temporary (typed-type $typed)))
@@ -22,8 +30,3 @@
   : (Stackof Identifier)
   (map typed-syntax-generate-temporary $typed-syntax-stack))
 
-(check-equal?
-  (string-prefix?
-    (symbol->string (syntax-e (type-generate-temporary (field `foo null))))
-    (symbol->string (type-symbol (field `foo null))))
-  #t)
