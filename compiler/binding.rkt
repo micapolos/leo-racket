c876bda8efb9d622aa3a4fdbe44b770a0e537c46
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-02-28 07:41:46 +0100

    typed-syntax-stack->typed-syntax

--- compiler/binding.rkt
+++ compiler/binding.rkt
@@ -3,6 +3,7 @@
 (provide (all-defined-out))
 
 (require 
+  leo/typed/stack
   leo/typed/option
   leo/typed/base
   leo/typed/type
@@ -15,7 +16,7 @@
   leo/typed/testing)
 
 (data binding
-  (param-type-list : (Listof Type))
+  (param-type-stack : (Stackof Type))
   (return-type : Type)
   (identifier : Identifier)
   (function? : Boolean))
@@ -25,79 +26,79 @@
 (define 
   (binding-resolve 
     ($binding : Binding) 
-    ($syntax-list : (Listof Syntax))) 
+    ($syntax-stack : (Stackof Syntax))) 
   : (Option Syntax)
   (and
     (types-match?
-      (map syntax-type $syntax-list)
-      (binding-param-type-list $binding))
+      (map syntax-type $syntax-stack)
+      (binding-param-type-stack $binding))
     (syntax-with-type
       (datum->syntax #f
         (if (binding-function? $binding)
           `(,(binding-identifier $binding)
-            ,@(filter syntax-is-dynamic? $syntax-list))
+            ,@(reverse (filter syntax-is-dynamic? $syntax-stack)))
           (binding-identifier $binding)))
       (binding-return-type $binding))))
 
 (check-equal?
   (option-map
     (binding-resolve 
-      (binding (list number-type string-type) boolean-type #`foo #f)
-      (list (syntax-with-type #`a number-type) (syntax-with-type #`b string-type)))
+      (binding (stack number-type string-type) boolean-type #`foo #f)
+      (stack (syntax-with-type #`a number-type) (syntax-with-type #`b string-type)))
     syntax-typed-datum)
   (typed `foo boolean-type))
 
 (check-equal?
   (option-map
     (binding-resolve 
-      (binding (list number-type string-type) boolean-type #`foo #t)
-      (list (syntax-with-type #`a number-type) (syntax-with-type #`b string-type)))
+      (binding (stack number-type string-type) boolean-type #`foo #t)
+      (stack (syntax-with-type #`a number-type) (syntax-with-type #`b string-type)))
     syntax-typed-datum)
   (typed `(foo a b) boolean-type))
 
 (check-equal?
   (binding-resolve 
-    (binding (list number-type string-type) boolean-type #`foo #f)
-    (list (syntax-with-type #`a number-type) (syntax-with-type #`b number-type)))
+    (binding (stack number-type string-type) boolean-type #`foo #f)
+    (stack (syntax-with-type #`a number-type) (syntax-with-type #`b number-type)))
   #f)
 
 ; ----------------------------------------------------------------------------
 
 (define
-  (binding-list-resolve
-    ($binding-list : (Listof Binding))
-    ($syntax-list : (Listof Syntax)))
+  (binding-stack-resolve
+    ($binding-stack : (Stackof Binding))
+    ($syntax-stack : (Stackof Syntax)))
   : (Option Syntax)
   (and
-    (not (null? $binding-list))
+    (not (null? $binding-stack))
     (or
-      (binding-resolve (car $binding-list) $syntax-list)
-      (binding-list-resolve (cdr $binding-list) $syntax-list))))
+      (binding-resolve (car $binding-stack) $syntax-stack)
+      (binding-stack-resolve (cdr $binding-stack) $syntax-stack))))
 
 (check-equal?
   (option-map
-    (binding-list-resolve 
-      (list 
-        (binding (list number-type) boolean-type #`number->boolean #t)
-        (binding (list string-type) boolean-type #`string->boolean #t))
-      (list (syntax-with-type #`a number-type)))
+    (binding-stack-resolve 
+      (stack 
+        (binding (stack number-type) boolean-type #`number->boolean #t)
+        (binding (stack string-type) boolean-type #`string->boolean #t))
+      (stack (syntax-with-type #`a number-type)))
     syntax-typed-datum)
   (typed `(number->boolean a) boolean-type))
 
 (check-equal?
   (option-map
-    (binding-list-resolve 
-      (list 
-        (binding (list number-type) boolean-type #`number->boolean #t)
-        (binding (list string-type) boolean-type #`string->boolean #t))
-      (list (syntax-with-type #`a string-type)))
+    (binding-stack-resolve 
+      (stack 
+        (binding (stack number-type) boolean-type #`number->boolean #t)
+        (binding (stack string-type) boolean-type #`string->boolean #t))
+      (stack (syntax-with-type #`a string-type)))
     syntax-typed-datum)
   (typed `(string->boolean a) boolean-type))
 
 (check-equal?
-  (binding-list-resolve 
-    (list 
-      (binding (list number-type) boolean-type #`number->boolean #t)
-      (binding (list string-type) boolean-type #`string->boolean #t))
-    (list (syntax-with-type #`a boolean-type)))
+  (binding-stack-resolve 
+    (stack 
+      (binding (stack number-type) boolean-type #`number->boolean #t)
+      (binding (stack string-type) boolean-type #`string->boolean #t))
+    (stack (syntax-with-type #`a boolean-type)))
   #f)
