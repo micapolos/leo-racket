cbe83d18b25fed41624d97d3b6d129094f3bb5c7
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-02-28 20:35:36 +0100

    type-symbol

--- compiler/generate-temporary.rkt
+++ compiler/generate-temporary.rkt
@@ -3,18 +3,27 @@
 (provide (all-defined-out))
 
 (require
+  racket/string
   leo/typed/stack
+  leo/typed/testing
+  leo/compiler/racket
   leo/compiler/type
+  leo/compiler/type-symbol
   leo/compiler/typed)
 
-; TODO: Based on type symbol
-(define (type-generate-temporary ($type : Type)) : Syntax
-  (car (generate-temporaries `(tmp))))
+(define (type-generate-temporary ($type : Type)) : Identifier
+  (car (generate-temporaries (list (type-symbol $type)))))
 
-(define (typed-syntax-generate-temporary ($typed : (Typed Syntax Type))) : Syntax
+(define (typed-syntax-generate-temporary ($typed : (Typed Syntax Type))) : Identifier
   (type-generate-temporary (typed-type $typed)))
 
 (define (typed-syntax-stack-generate-temporaries 
   ($typed-syntax-stack : (Stackof (Typed Syntax Type)))) 
-  : (Stackof Syntax)
+  : (Stackof Identifier)
   (map typed-syntax-generate-temporary $typed-syntax-stack))
+
+(check-equal?
+  (string-prefix?
+    (symbol->string (syntax-e (type-generate-temporary (field `foo null))))
+    (symbol->string (type-symbol (field `foo null))))
+  #t)
---
+++ compiler/type-symbol.rkt
@@ -0,0 +1,25 @@
+#lang typed/racket/base
+
+(provide (all-defined-out))
+
+(require
+  leo/typed/testing
+  leo/compiler/racket
+  leo/compiler/type)
+
+(define (type-symbol ($type : Type)) : Symbol
+  (cond
+    ((field? $type) (field-symbol $type))
+    ((racket? $type) 
+      (define $any (racket-any $type))
+      (cond
+        ((symbol? $any) $any)
+        (else `any)))
+    ((arrow? $type) `function)
+    ((a? $type) `type)))
+
+(check-equal? (type-symbol (field `foo null)) `foo)
+(check-equal? (type-symbol (racket `foo)) `foo)
+(check-equal? (type-symbol (racket 123)) `any)
+(check-equal? (type-symbol (arrow null (racket `foo))) `function)
+(check-equal? (type-symbol (a (racket `foo))) `type)
