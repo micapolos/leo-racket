0f0b184d719d8e398c3eb1c3a7a4570992f49abc
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-01 07:39:20 +0100

    set-srcloc

--- compiler/srcloc.rkt
+++ compiler/srcloc.rkt
@@ -2,6 +2,10 @@
 
 (provide (all-defined-out))
 
+(require
+  leo/typed/base
+  leo/typed/testing)
+
 (define empty-srcloc (srcloc #f #f #f #f #f))
 (define test-srcloc (srcloc "test.leo" 1 2 3 4))
 (define srcloc-a (srcloc "a.leo" 1 2 3 4))
@@ -9,4 +13,15 @@
 (define srcloc-c (srcloc "c.leo" 1 2 3 4))
 (define srcloc-d (srcloc "d.leo" 1 2 3 4))
 
-(define srcloc-weak-hash make-weak-hash)
+(define srcloc-weak-hash : (Weak-HashTable Any srcloc) (make-weak-hash))
+
+(define (set-srcloc! ($any : Any) ($srcloc : srcloc))
+  (hash-set! srcloc-weak-hash $any $srcloc))
+
+(define (any-srcloc ($any : Any)) : (Option srcloc)
+  (hash-ref srcloc-weak-hash $any (lambda () #f)))
+
+(bind $obj (gensym)
+  (check-equal? (any-srcloc $obj) #f)
+  (set-srcloc! $obj srcloc-a)
+  (check-equal? (any-srcloc $obj) srcloc-a))
