49506fdff2d9245476865cf8247c8ef4ba7bb14d
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-01 02:51:12 +0100

    expression-utils

--- compiler/expression-utils.rkt
+++ compiler/expression-utils.rkt
@@ -3,8 +3,11 @@
 (provide (all-defined-out))
 
 (require
+  leo/typed/stack
+  leo/typed/testing
   leo/compiler/expression
   leo/compiler/syntax-utils
+  leo/compiler/type
   leo/compiler/type-utils)
 
 (define expression-a (expression syntax-a type-a))
@@ -14,3 +17,27 @@
 
 (define (expression-is-dynamic? ($expression : Expression)) : Boolean
   (type-is-dynamic? (expression-type $expression)))
+
+(define (expression-stack-type-stack 
+  ($expression-stack : (Stackof Expression)))
+  : (Stackof Type)
+  (map expression-type $expression-stack))
+
+(define (expression-stack-syntax-stack
+  ($expression-stack : (Stackof Expression)))
+  : (Stackof Syntax)
+  (map expression-syntax $expression-stack))
+
+(define (expression-stack-dynamic-syntax-stack 
+  ($expression-stack : (Stackof Expression)))
+  : (Stackof Syntax)
+  (expression-stack-syntax-stack
+    (filter expression-is-dynamic? $expression-stack)))
+
+(check-equal?
+  (expression-stack-type-stack (stack expression-a expression-b))
+  (stack type-a type-b))
+
+(check-equal?
+  (expression-stack-syntax-stack (stack expression-a expression-b))
+  (stack syntax-a syntax-b))
