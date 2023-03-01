6099d9c29d722fd9a78f54436ca60fdc9d71a475
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-01 22:54:13 +0100

    expression-context-push

--- compiler/expression-context.rkt
+++ compiler/expression-context.rkt
@@ -7,7 +7,35 @@
   leo/typed/stack)
 
 (struct expression-context (
-  ($private-expression-stack : (Stackof Expression))
-  ($public-expression-stack : (Stackof Expression)))
+  (private-expression-stack : (Stackof Expression))
+  (public-expression-stack : (Stackof Expression)))
   #:transparent
   #:type-name Expression-Context)
+
+(define (expression-context-push-private 
+  ($expression-context : Expression-Context)
+  ($expression : Expression))
+  : Expression-Context
+  (struct-copy expression-context $expression-context
+    (private-expression-stack 
+      (push
+        (expression-context-private-expression-stack $expression-context)
+        $expression))))
+
+(define (expression-context-push-public 
+  ($expression-context : Expression-Context)
+  ($expression : Expression))
+  : Expression-Context
+  (struct-copy expression-context $expression-context
+    (public-expression-stack 
+      (push
+        (expression-context-public-expression-stack $expression-context)
+        $expression))))
+
+(define (expression-context-push 
+  ($expression-context : Expression-Context)
+  ($expression : Expression))
+  : Expression-Context
+  (expression-context-push-public
+    (expression-context-push-private $expression-context $expression)
+    $expression))
