e3db3c1a42174037572837b67ddb5abab09358a7
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-01 23:01:40 +0100

    expression-stacks

--- compiler/expression-stacks.rkt
+++ compiler/expression-stacks.rkt
@@ -12,7 +12,7 @@
   #:transparent
   #:type-name Expression-Stacks)
 
-(define (expression-stacks-push-private 
+(define (expression-stacks-private-push
   ($expression-stacks : Expression-Stacks)
   ($expression : Expression))
   : Expression-Stacks
@@ -22,7 +22,30 @@
         (expression-stacks-private $expression-stacks)
         $expression))))
 
-(define (expression-stacks-push-public 
+
+(define (expression-stacks-private-push-stack
+  ($expression-stacks : Expression-Stacks)
+  ($expression-stack : (Stackof Expression)))
+  : Expression-Stacks
+  (struct-copy expression-stacks $expression-stacks
+    (private 
+      (append 
+        $expression-stack
+        (expression-stacks-private $expression-stacks)))))
+
+(define (expression-stacks-push-stack
+  ($expression-stacks : Expression-Stacks)
+  ($expression-stack : (Stackof Expression)))
+  : Expression-Stacks
+  (expression-stacks
+    (append 
+      $expression-stack
+      (expression-stacks-private $expression-stacks))
+    (append 
+      $expression-stack
+      (expression-stacks-public $expression-stacks))))
+
+(define (expression-stacks-public-push
   ($expression-stacks : Expression-Stacks)
   ($expression : Expression))
   : Expression-Stacks
@@ -36,6 +59,12 @@
   ($expression-stacks : Expression-Stacks)
   ($expression : Expression))
   : Expression-Stacks
-  (expression-stacks-push-public
-    (expression-stacks-push-private $expression-stacks $expression)
+  (expression-stacks-public-push
+    (expression-stacks-private-push $expression-stacks $expression)
     $expression))
+
+(define (expression-stacks-begin
+  ($expression-stacks : Expression-Stacks))
+  : Expression-Stacks
+  (struct-copy expression-stacks $expression-stacks
+    (public null)))
