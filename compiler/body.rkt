625fe491634e36ee3e679f4fccb942eef9b18011
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-02 19:35:34 +0100

    body-commit-values

--- compiler/body.rkt
+++ compiler/body.rkt
@@ -7,8 +7,9 @@
   leo/typed/stack
   leo/typed/testing
   leo/compiler/syntax-utils
+  leo/compiler/generate-temporary
   leo/compiler/identifier-utils
-  leo/compiler/expression-utils
+  leo/compiler/expression
   leo/compiler/type-utils
   leo/compiler/binding
   leo/compiler/block
@@ -39,8 +40,36 @@
   (map syntax->datum
     (body-syntax-list 
       (body
-         (block 
-            (stack (make-syntax `(define pi 3.14)))
-            (stack (binding type-c identifier-c)))
-         (values (stack expression-c expression-d)))))
-  `((define pi 3.14) (values c d)))
+        (block 
+          (stack 
+            (make-syntax `(define-values (zero pi) (values 0 3.14)))
+            (make-syntax `(define (plus a b) (+ a b))))
+          (stack (binding type-c identifier-c)))
+        (values 
+          (stack 
+            (expression (make-syntax `pi) type-a)
+            (expression (make-syntax `(plus pi 2)) type-b))))))
+  `(
+    (define-values (zero pi) (values 0 3.14))
+    (define (plus a b) (+ a b)) 
+    (values pi (plus pi 2))))
+
+(parameterize ((tmp-temporaries? #t))
+  (check-equal?
+    (map syntax->datum
+      (block-syntax-list 
+        (body-as-block
+          (body
+            (block 
+              (stack 
+                (make-syntax `(define-values (zero pi) (values 0 3.14)))
+                (make-syntax `(define (plus a b) (+ a b))))
+              (stack (binding type-c identifier-c)))
+           (values 
+              (stack 
+                (expression (make-syntax `pi) type-a)
+                (expression (make-syntax `(plus pi 2)) type-b)))))))
+    `(
+      (define-values (zero pi) (values 0 3.14))
+      (define (plus a b) (+ a b)) 
+      (define-values (tmp-a tmp-b) (values pi (plus pi 2))))))
