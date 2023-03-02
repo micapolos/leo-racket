1f4d0ed734c9e3db8f5841891885e63ccf4f5b26
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-02 13:51:36 +0100

    block-syntax-list

--- compiler/block.rkt
+++ compiler/block.rkt
@@ -4,10 +4,22 @@
 
 (require 
   leo/typed/stack
-  leo/compiler/type)
+  leo/typed/testing
+  leo/compiler/type
+  leo/compiler/syntax-utils)
 
 (struct block (
-  ($syntax-stack : (Stackof Syntax))
-  ($type-stack : (Stackof Type)))
+  (syntax-stack : (Stackof Syntax))
+  (type-stack : (Stackof Type)))
   #:transparent
   #:type-name Block)
+
+(define (block-syntax-list ($block : Block)) : (Stackof Syntax)
+  (reverse (block-syntax-stack $block)))
+
+(check-equal?
+  (map
+    syntax->datum
+    (block-syntax-list
+      (block (stack syntax-a syntax-b) null)))
+  (list `a `b))
