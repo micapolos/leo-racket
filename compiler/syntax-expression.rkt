0988f1813febe37a9d8b60efe373ba6e837528f8
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-03 02:38:11 +0100

    compiler-append-syntax

--- compiler/syntax-expression.rkt
+++ compiler/syntax-expression.rkt
@@ -3,11 +3,28 @@
 (provide (all-defined-out))
 
 (require 
+  leo/typed/option
+  leo/typed/testing
+  leo/compiler/syntax-utils
   leo/compiler/expression
-  leo/compiler/expression-utils)
+  leo/compiler/expression-utils
+  leo/compiler/type-utils
+  leo/compiler/typed)
 
 (define (syntax-expression-option ($syntax : Syntax)) : (Option Expression)
   (define $syntax-e (syntax-e $syntax))
   (or
     (and (number? $syntax-e) (number-expression $syntax-e))
     (and (string? $syntax-e) (text-expression $syntax-e))))
+
+(check-equal?
+  (option-map 
+    (syntax-expression-option (make-syntax 1)) 
+    expression-typed-datum)
+  (typed 1 number-type))
+
+(check-equal?
+  (option-map 
+    (syntax-expression-option (make-syntax "foo")) 
+    expression-typed-datum)
+  (typed "foo" text-type))
