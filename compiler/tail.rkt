1a76cd175c48581fd4a707f67a176867bcabc22f
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-03 12:54:05 +0100

    tail

---
+++ compiler/tail.rkt
@@ -0,0 +1,10 @@
+#lang typed/racket/base
+
+(provide (all-defined-out))
+
+(require
+  leo/typed/stack
+  leo/compiler/expression
+  leo/compiler/expressions)
+
+(define-type Tail (U (Stackof Expression) Expressions))
