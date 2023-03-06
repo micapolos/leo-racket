50a5e44f2fadb036d692a8f205561490977b3518
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-06 02:17:52 +0100

    leo sandbox

--- compiler/base-scope.rkt
+++ compiler/base-scope.rkt
@@ -13,10 +13,59 @@
     (binding
       (arrow
         (structure 
+          number-type
+          (field `plus (structure number-type)))
+        (structure number-type))
+      `+)
+    (binding
+      (arrow
+        (structure 
+          number-type
+          (field `minus (structure number-type)))
+        (structure number-type))
+      `-)
+    (binding
+      (arrow
+        (structure 
+          number-type
+          (field `times (structure number-type)))
+        (structure number-type))
+      `*)
+    (binding
+      (arrow
+        (structure 
+          number-type
+          (a text-type))
+        (structure text-type))
+      `number->string)
+    (binding
+      (arrow
+        (structure 
+          int-type
+          (a text-type))
+        (structure text-type))
+      `number->string)
+    (binding
+      (arrow
+        (structure 
           int-type
           (field `plus (structure int-type)))
         (structure int-type))
       `unsafe-fx+)
+    (binding
+      (arrow
+        (structure 
+          int-type
+          (field `minus (structure int-type)))
+        (structure int-type))
+      `unsafe-fx-)
+    (binding
+      (arrow
+        (structure 
+          int-type
+          (field `times (structure int-type)))
+        (structure int-type))
+      `unsafe-fx*)
     (binding
       (arrow
         (structure 
--- compiler/sandbox.rkt
+++ compiler/sandbox.rkt
@@ -1,42 +1,15 @@
 #lang typed/racket/base
 
-(require 
-  leo/typed/option
-  leo/typed/stack
-  leo/compiler/binding
-  leo/compiler/binding-resolve
-  leo/compiler/scope
-  leo/compiler/scope-resolve
-  leo/compiler/type
-  leo/compiler/type-utils
-  leo/compiler/syntax-utils
-  leo/compiler/expression
-  leo/compiler/expression-resolve
-  leo/compiler/expression-utils
-  leo/compiler/package-utils)
+(require leo/compiler/leo)
 
-(scope-resolve-tuple
-  (scope 
-    (binding
-      (arrow 
-        (structure text-type (field `plus (structure text-type)))
-        (structure text-type))
-      `string-append)
-    (binding
-      (arrow 
-        (structure int-type (field `text null-structure))
-        (structure text-type))
-      `number->string)
-    (binding
-      (arrow 
-        (structure int-type (field `plus (structure int-type)))
-        (structure int-type))
-      `unsafe-fx+)
-    (binding
-      (arrow 
-        (structure int-type (field `times (structure int-type)))
-        (structure int-type))
-      `unsafe-fx*))
-  (tuple
-    (int-expression 1)
-    (field-expression `text null-structure)))
+(leo
+  1
+  (plus 3)
+  text
+  (plus " ")
+  (plus "apples")
+  (do
+    text
+    (plus ", ")
+    (plus text)
+    (plus "!!!")))
