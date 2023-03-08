9143430f078c5e1176e089c6601c9e01771e3c2c
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-08 21:19:19 +0100

    recipe

--- compiler/type-symbol.rkt
+++ compiler/type-symbol.rkt
@@ -10,10 +10,10 @@
   (cond
     ((field? $type) (field-symbol $type))
     ((racket? $type) `racket)
-    ((arrow? $type) `function)
-    ((a? $type) `a)))
+    ((arrow? $type) `recipe)
+    ((a? $type) `type)))
 
 (check-equal? (type-symbol (field `foo null)) `foo)
 (check-equal? (type-symbol (racket)) `racket)
-(check-equal? (type-symbol (arrow null null)) `function)
-(check-equal? (type-symbol (a (racket))) `a)
+(check-equal? (type-symbol (arrow null null)) `recipe)
+(check-equal? (type-symbol (a (racket))) `type)
