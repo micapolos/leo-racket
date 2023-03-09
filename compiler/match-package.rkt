3f62f185a6f6cc24c142143406e238c241c8e583
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-10 00:12:52 +0100

    match-package

---
+++ compiler/match-package.rkt
@@ -0,0 +1,17 @@
+#lang typed/racket/base
+
+(provide (all-defined-out))
+
+(require
+  leo/typed/base
+  leo/typed/stack
+  leo/compiler/type
+  leo/compiler/expressions)
+
+(data match-package
+  (case-stack : (Stackof Case))
+  (type-list : (Listof Type)))
+
+(data case
+  (lhs-type : Type)
+  (rhs-expressions : Expressions))
