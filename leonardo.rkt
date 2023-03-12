2892642c5fe99098c164213735ec43c7d48b8013
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-12 13:56:05 +0100

    leonardo number :)

--- leonardo.rkt
+++ leonardo.rkt
@@ -2,6 +2,15 @@
 
 (provide (all-defined-out))
 
+(define (fibonacci number)
+  (if (< number 2) 
+    number 
+    (+
+      (fibonacci (- number 2)) 
+      (fibonacci (- number 1)))))
+
+(time (fibonacci 42))
+
 (define (leonardo number)
   (if (< number 2) 
     1
