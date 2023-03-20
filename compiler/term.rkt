8b76d3ddfebaa06809f17e6c16ec99d5efe1e080
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-03-20 02:08:36 +0100

    index / switch

--- compiler/term.rkt
+++ compiler/term.rkt
@@ -4,6 +4,7 @@
 
 (require
   racket/function
+  racket/list
   leo/typed/base
   leo/typed/stack
   leo/typed/testing
@@ -14,11 +15,29 @@
 (define-type Term 
   (U
     Syntax
+    Index
+    Tuple
+    Tuple-Ref
+    Switch
     Variable
     Abstraction
     Application
-    Binder
-    Conditional))
+    Binder))
+
+(data index
+  (value : Exact-Nonnegative-Integer)
+  (size : Exact-Nonnegative-Integer))
+
+(data tuple
+  (term-stack : (Stackof Term)))
+
+(data tuple-ref
+  (term : Term)
+  (index : Index))
+
+(data switch
+  (term : Term)
+  (case-stack : (Stackof Term)))
 
 (data variable
   (index : Exact-Nonnegative-Integer))
@@ -40,17 +59,22 @@
   (binding-stack : (Stackof Binding))
   (body : Term))
 
-(data conditional
-  (condition : Term)
-  (consequent : Term)
-  (alternate : Term))
-
 (define-type Recursiveness 
   (U 
     'non-recursive 
     'recursive))
 
 ; -----------------------------------------------------------------------------
+
+(define-syntax (tuple! $syntax)
+  (syntax-case $syntax ()
+    ((_ term ...)
+      #`(tuple (stack term ...)))))
+
+(define-syntax (switch! $syntax)
+  (syntax-case $syntax ()
+    ((_ term case ...)
+      #`(switch term (stack case ...)))))
 
 (define v0 (variable 0))
 (define v1 (variable 1))
@@ -94,11 +118,6 @@
         (stack #,@(map (lambda (s) #`(binding! #,@s)) (syntax-e #`(binding ...))))
         body))))
 
-(define-syntax (if! $syntax)
-  (syntax-case $syntax ()
-    ((_ condition consequent alternate)
-      #`(conditional condition consequent alternate))))
-
 ; -------------------------------------------------------------------------------------
 
 (define (term-sexp ($term : Term)) : Sexp
@@ -113,6 +132,62 @@
 : Syntax
   (cond
     ((syntax? $term) $term)
+    ((index? $term)
+      (define $value (index-value $term))
+      (make-syntax
+        (case (index-size $term)
+          ((0) (error "impossible"))
+          ((2) (= $value 0))
+          (else $value))))
+    ((tuple? $term)
+      (define $term-stack (tuple-term-stack $term))
+      (define $size (length $term-stack))
+      (make-syntax
+        (case $size
+          ((0) `())
+          ((1) 
+            (identifier-stack-term-syntax $identifier-stack (car $term-stack)))
+          (else
+            `(
+              ,(if (= $size 2) `cons `vector)
+              ,@(reverse
+                (map 
+                  (curry identifier-stack-term-syntax $identifier-stack)
+                  $term-stack)))))))
+    ((tuple-ref? $term) 
+      (define $syntax (identifier-stack-term-syntax $identifier-stack (tuple-ref-term $term)))
+      (define $index (tuple-ref-index $term))
+      (define $size (index-size $index))
+      (define $value (index-value $index))
+      (make-syntax
+        (case $size
+          ((0) (error "impossible"))
+          ((1) $syntax)
+          ((2) `(,(if (= $value 0) `car `cdr) ,$syntax))
+          (else `(vector-ref ,$syntax ,$value)))))
+    ((switch? $term) 
+      (define $selector-syntax (identifier-stack-term-syntax $identifier-stack (switch-term $term)))
+      (define $case-syntax-stack 
+        (map 
+          (curry identifier-stack-term-syntax $identifier-stack)
+          (switch-case-stack $term)))
+      (define $size (length $case-syntax-stack))
+      (make-syntax
+        (case $size
+          ((0) (error "impossible"))
+          ((1) (top $case-syntax-stack))
+          ((2) 
+            `(if ,$selector-syntax 
+              ,(pop-top $case-syntax-stack) 
+              ,(top $case-syntax-stack)))
+          (else 
+            `(case ,$selector-syntax
+              ,@(map
+                  (lambda (($index : Exact-Nonnegative-Integer) ($case-syntax : Syntax))
+                    `((,$index) ,$case-syntax))
+                  (range (sub1 $size))
+                  (reverse (cdr $case-syntax-stack)))
+              (else ,(top $case-syntax-stack)))))))
     ((variable? $term)
       (stack-ref $identifier-stack (variable-index $term)))
     ((abstraction? $term)
@@ -158,13 +233,7 @@
                 (map 
                   (ann car (-> (Pairof (Stackof Identifier) Syntax) (Stackof Identifier)))
                   $tmp-stack-syntax-pair-stack)))
-            (binder-body $term)))))
-    ((conditional? $term)
-      (make-syntax
-        `(if
-          ,(identifier-stack-term-syntax $identifier-stack (conditional-condition $term))
-          ,(identifier-stack-term-syntax $identifier-stack (conditional-consequent $term))
-          ,(identifier-stack-term-syntax $identifier-stack (conditional-alternate $term)))))))
+            (binder-body $term)))))))
 
 (define (identifier-stack-binding-tmp-stack-syntax-pair
   ($identifier-stack : (Stackof Identifier))
@@ -179,9 +248,76 @@
 
 ; --------------------------------------------------------------------------------
 
+(check-equal? (term-sexp (index 0 1)) 0)
+
+(check-equal? (term-sexp (index 0 2)) #t)
+(check-equal? (term-sexp (index 1 2)) #f)
+
+(check-equal? (term-sexp (index 0 3)) 0)
+(check-equal? (term-sexp (index 1 3)) 1)
+(check-equal? (term-sexp (index 2 3)) 2)
+
 (check-equal?
-  (syntax->datum (term-syntax #`some-syntax))
-  `some-syntax)
+  (syntax->datum (term-syntax #`foo))
+  `foo)
+
+(check-equal?
+  (term-sexp (tuple!))
+  `())
+
+(check-equal?
+  (term-sexp (tuple! #`a))
+  `a)
+
+(check-equal?
+  (term-sexp (tuple! #`a #`b))
+  `(cons a b))
+
+(check-equal?
+  (term-sexp (tuple! #`a #`b #`c))
+  `(vector a b c))
+
+(check-equal?
+  (term-sexp (tuple-ref #`tuple (index 0 1)))
+  `tuple)
+
+(check-equal?
+  (term-sexp (tuple-ref #`tuple (index 0 2)))
+  `(car tuple))
+
+(check-equal?
+  (term-sexp (tuple-ref #`tuple (index 1 2)))
+  `(cdr tuple))
+
+(check-equal?
+  (term-sexp (tuple-ref #`tuple (index 0 3)))
+  `(vector-ref tuple 0))
+
+(check-equal?
+  (term-sexp (tuple-ref #`tuple (index 1 3)))
+  `(vector-ref tuple 1))
+
+(check-equal?
+  (term-sexp (tuple-ref #`tuple (index 2 3)))
+  `(vector-ref tuple 2))
+
+(check-equal?
+  (term-sexp 
+    (switch! #`selector #`a))
+  `a)
+
+(check-equal?
+  (term-sexp 
+    (switch! #`selector #`a #`b))
+  `(if selector a b))
+
+(check-equal?
+  (term-sexp 
+    (switch! #`selector #`a #`b #`c))
+  `(case selector 
+    ((0) a)
+    ((1) b)
+    (else c)))
 
 (check-equal?
   (term-sexp (lambda! (a b c) (app! v2 v1 v0)))
@@ -207,7 +343,3 @@
     (((tmp-a tmp-b) term-1) 
      ((tmp-c tmp-d) term-2))
     (tmp-a tmp-b tmp-c tmp-d)))
-
-(check-equal?
-  (term-sexp (if! #`a #`b #`c))
-  `(if a b c))
