0b844e1e7734608b1ab3be2ad54b420f1aa77f84
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-04-04 02:05:40 +0200

    failure-plus

--- typed/failure.rkt
+++ typed/failure.rkt
@@ -10,6 +10,28 @@
 (data (V) failure
   (value : V))
 
+(define null-failure : (Failure Null)
+  (failure null))
+
+(define #:forall (F I) (failure-plus ($failure : (Failure F)) ($item : I)) : (Failure (Pairof I F))
+  (failure (pair $item (failure-value $failure))))
+
+(define #:forall (V) (top-failure ($value : V)) : (Failure (List V))
+  (failure-plus null-failure $value))
+
+(define-syntax (failure! $syntax)
+  (syntax-case $syntax ()
+    ((_ $value ...)
+      (foldl
+        (lambda ($syntax $failure)
+          #`(failure-plus #,$failure #,$syntax))
+        #`null-failure
+        (syntax-e #`($value ...))))))
+
+(check-equal? (failure!) null-failure)
+(check-equal? (failure! 1) (failure-plus null-failure 1))
+(check-equal? (failure! 1 2) (failure-plus (failure-plus null-failure 1) 2))
+
 (define-syntax (or-failure $syntax)
   (syntax-case $syntax ()
     ((_ body ...)
