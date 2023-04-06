dfcbe4026820f68b1e2598251527db1b794bd9be
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-04-06 02:44:55 +0200

    sexp-parser

--- parser/sentence-parser.rkt
+++ parser/sentence-parser.rkt
@@ -3,21 +3,17 @@
 (require
   leo/parser/literal-parser)
 
-(data (V I) env
-  (begin-parser-fn :
-    (->
-      V ; currently parsed value
-      Symbol ; begin symbol
-      (->
-        I ; starting inner value
-        (-> I (Parser I)) ; inner item parser
-        (-> I V) ; end function, accumulating inner value into outer value
-        (Parser V))
-      (Parser V))))
+(data (V) env
+  (custom-parser-fn : (-> V (Parser V)))
+  (begin-parser-fn : (-> V Symbol (-> V (-> V V) (Parser V)) (Parser V))))
 
-(: env-begin-parser : (All (V I) (-> (Env V I) V Symbol (-> I (-> I (Parser I)) (-> I V) (Parser V)) (Parser V))))
-(define (env-begin-parser $env $value $symbol $item-parser-fn)
-  (#%app (env-begin-parser-fn $env) $value $symbol $item-parser-fn))
+(: env-custom-parser : (All (V) (-> (Env V) V (Parser V))))
+(define (env-custom-parser $env $value)
+  (#%app (env-custom-parser-fn $env) $value))
+
+(: env-begin-parser : (All (V) (-> (Env V) V Symbol (-> V (-> V V) (Parser V)) (Parser V))))
+(define (env-begin-parser $env $value $symbol $inner-parser-fn)
+  (#%app (env-begin-parser-fn $env) $value $symbol $inner-parser-fn))
 
 ; --------------------------------------------------------------------------------------
 
@@ -30,182 +26,92 @@
 (define comma-or-newlines-separator-parser
   (parser-or comma-separator-parser newlines-separator-parser))
 
-(: env-script-parser : (All (V I) (-> (Env V I) V (-> V (Parser V)) (Parser V))))
-(define (env-script-parser $env $value $custom-parser-fn)
+(: env-script-parser : (All (V) (-> (Env V) V (Parser V))))
+(define (env-script-parser $env $value)
   (prefix-parser maybe-newlines-parser
     (parser-or
       (parser $value)
       (parser-suffix
         (repeat-separated-parser $value comma-or-newlines-separator-parser
           (lambda (($repeated-value : V))
-            (parser-or
-              ($custom-parser-fn $repeated-value)
-              (env-sentence-parser $env $repeated-value))))
+            (env-line-parser $env $repeated-value)))
         newlines-parser))))
 
-(: env-sentence-parser : (All (V I) (-> (Env V I) V (Parser V))))
+(: env-line-parser : (All (V) (-> (Env V) V (Parser V))))
+(define (env-line-parser $env $value)
+  (parser-or
+    (env-custom-parser $env $value)
+    (env-sentence-parser $env $value)))
+
+(: env-sentence-parser : (All (V) (-> (Env V) V (Parser V))))
 (define (env-sentence-parser $env $value)
   (parser-bind word-parser
     (lambda (($word : Word))
       (env-rhs-parser $env $value
         (word-symbol $word)))))
 
-(: env-rhs-parser : (All (V I) (-> (Env V I) V Symbol (Parser V))))
+(: env-rhs-parser : (All (V) (-> (Env V) V Symbol (Parser V))))
 (define (env-rhs-parser $env $value $symbol)
   (parser-or
     (env-symbol-parser $env $value $symbol)
-    (env-dot-rhs-parser $env $value $symbol)
     (env-space-rhs-parser $env $value $symbol)
     (env-colon-rhs-parser $env $value $symbol)
     (env-parens-rhs-parser $env $value $symbol)
     (env-newline-rhs-parser $env $value $symbol)))
 
-(: env-symbol-parser : (All (V I) (-> (Env V I) V Symbol (Parser V))))
+(: env-symbol-parser : (All (V) (-> (Env V) V Symbol (Parser V))))
 (define (env-symbol-parser $env $value $symbol)
   (env-begin-parser $env $value $symbol
-    (lambda (($item : I) ($item-parser-fn : (-> I (Parser I))) ($end-fn : (-> I V))) : (Parser V)
-      (parser (#%app $end-fn $item)))))
+    (lambda (($rhs : V) ($end-fn : (-> V V))) : (Parser V)
+      (parser (#%app $end-fn $rhs)))))
 
-(: env-dot-rhs-parser : (All (V I) (-> (Env V I) V Symbol (Parser V))))
-(define (env-dot-rhs-parser $env $value $symbol)
-  (prefix-parser (exact-char-parser #\.)
-    (env-begin-parser $env $value $symbol
-      (lambda (($item : I) ($item-parser-fn : (-> I (Parser I))) ($end-fn : (-> I V))) : (Parser V)
-        (env-sentence-parser $env (#%app $end-fn $item))))))
-
-(: env-space-rhs-parser : (All (V I) (-> (Env V I) V Symbol (Parser V))))
+(: env-space-rhs-parser : (All (V) (-> (Env V) V Symbol (Parser V))))
 (define (env-space-rhs-parser $env $value $symbol)
   (prefix-parser space-parser
     (env-begin-parser $env $value $symbol
-      (lambda (($item : I) ($item-parser-fn : (-> I (Parser I))) ($end-fn : (-> I V))) : (Parser V)
+      (lambda (($rhs : V) ($end-fn : (-> V V))) : (Parser V)
         (parser-map
-          (#%app $item-parser-fn $item)
-          (lambda (($parsed-item : I))
-            (#%app $end-fn $parsed-item)))))))
+          (env-line-parser $env $rhs)
+          (lambda (($parsed-rhs : V))
+            (#%app $end-fn $parsed-rhs)))))))
 
-(: env-colon-rhs-parser : (All (V I) (-> (Env V I) V Symbol (Parser V))))
+(: env-colon-rhs-parser : (All (V) (-> (Env V) V Symbol (Parser V))))
 (define (env-colon-rhs-parser $env $value $symbol)
   (prefix-parser (exact-string-parser ": ")
     (env-begin-parser $env $value $symbol
-      (lambda (($item : I) ($item-parser-fn : (-> I (Parser I))) ($end-fn : (-> I V))) : (Parser V)
+      (lambda (($rhs : V) ($end-fn : (-> V V))) : (Parser V)
         (parser-map
-          (repeat-separated-parser $item comma-separator-parser
-            (lambda (($following-item : I))
-              (#%app $item-parser-fn $following-item)))
-          (lambda (($parsed-item : I))
-            (#%app $end-fn $parsed-item)))))))
+          (repeat-separated-parser $rhs comma-separator-parser
+            (lambda (($repeated-rhs : V))
+              (env-line-parser $env $repeated-rhs)))
+          (lambda (($parsed-rhs : V))
+            (#%app $end-fn $parsed-rhs)))))))
 
-(: env-parens-rhs-parser : (All (V I) (-> (Env V I) V Symbol (Parser V))))
+(: env-parens-rhs-parser : (All (V) (-> (Env V) V Symbol (Parser V))))
 (define (env-parens-rhs-parser $env $value $symbol)
   (prefix-parser-suffix
     (exact-string-parser "(")
     (env-begin-parser $env $value $symbol
-      (lambda (($item : I) ($item-parser-fn : (-> I (Parser I))) ($end-fn : (-> I V))) : (Parser V)
+      (lambda (($rhs : V) ($end-fn : (-> V V))) : (Parser V)
         (parser-or
-          (parser (#%app $end-fn $item))
+          (parser (#%app $end-fn $rhs))
           (parser-map
-            (repeat-separated-parser $item comma-separator-parser
-              (lambda (($following-item : I))
-                (#%app $item-parser-fn $following-item)))
-            (lambda (($parsed-item : I))
-              (#%app $end-fn $parsed-item))))))
+            (repeat-separated-parser $rhs comma-separator-parser
+              (lambda (($repeated-rhs : V))
+                (env-line-parser $env $repeated-rhs)))
+            (lambda (($parsed-rhs : V))
+              (#%app $end-fn $parsed-rhs))))))
     (exact-string-parser ")")))
 
-(: env-newline-rhs-parser : (All (V I) (-> (Env V I) V Symbol (Parser V))))
+(: env-newline-rhs-parser : (All (V) (-> (Env V) V Symbol (Parser V))))
 (define (env-newline-rhs-parser $env $value $symbol)
   (prefix-parser newlines-parser
     (indented-parser
       (env-begin-parser $env $value $symbol
-        (lambda (($item : I) ($item-parser-fn : (-> I (Parser I))) ($end-fn : (-> I V))) : (Parser V)
+        (lambda (($rhs : V) ($end-fn : (-> V V))) : (Parser V)
           (parser-map
-            (repeat-separated-parser $item comma-or-newlines-separator-parser
-              (lambda (($following-item : I))
-                (#%app $item-parser-fn $following-item)))
-            (lambda (($parsed-item : I))
-              (#%app $end-fn $parsed-item))))))))
-
-(let ()
-  (define test-env : (Env (Stackof String) String)
-    (env
-      (lambda
-        (
-          ($string-stack : (Stackof String))
-          ($symbol : Symbol)
-          ($fn : (->
-            String
-            (-> String (Parser String))
-            (-> String (Stackof String))
-            (Parser (Stackof String)))))
-        : (Parser (Stackof String))
-        (#%app $fn
-          (string-append
-            (number->string (length $string-stack))
-            "-"
-            (symbol->string $symbol))
-          (lambda (($string : String)) : (Parser String)
-            (parser-map (non-empty-stack-parser numeric-char-parser)
-              (lambda (($char-stack : (Stackof Char)))
-                (string-append $string "+"
-                  (list->string (reverse $char-stack))))))
-          (lambda (($string : String)) : (Stackof String)
-            (push $string-stack $string))))))
-
-  (define sentence-parser : (Parser (Stackof String))
-    (env-sentence-parser test-env (stack "foo" "bar")))
-
-  (define script-parser : (Parser (Stackof String))
-    (env-script-parser test-env null
-      (lambda ((_ : (Stackof String)))
-        (failure! `custom))))
-
-  (check-equal? (parse sentence-parser "foo") (stack "foo" "bar" "2-foo"))
-  (check-equal? (parse sentence-parser "foo 123") (stack "foo" "bar" "2-foo+123"))
-
-  (check-equal? (parse sentence-parser "foo\n  123") (stack "foo" "bar" "2-foo+123"))
-  (check-equal? (parse sentence-parser "foo\n  123\n  456") (stack "foo" "bar" "2-foo+123+456"))
-
-  (check-equal? (parse sentence-parser "foo: 123") (stack "foo" "bar" "2-foo+123"))
-  (check-equal? (parse sentence-parser "foo: 123, 456") (stack "foo" "bar" "2-foo+123+456"))
-
-  (check-equal? (parse sentence-parser "foo()") (stack "foo" "bar" "2-foo"))
-  (check-equal? (parse sentence-parser "foo(123)") (stack "foo" "bar" "2-foo+123"))
-  (check-equal? (parse sentence-parser "foo(123, 456)") (stack "foo" "bar" "2-foo+123+456"))
-
-  (check-equal? (parse sentence-parser "goo.gar") (stack "foo" "bar" "2-goo" "3-gar"))
-  (check-equal? (parse sentence-parser "goo.gar 123") (stack "foo" "bar" "2-goo" "3-gar+123"))
-  (check-equal? (parse sentence-parser "goo.gar: 123") (stack "foo" "bar" "2-goo" "3-gar+123"))
-  (check-equal? (parse sentence-parser "goo.gar: 123, 456") (stack "foo" "bar" "2-goo" "3-gar+123+456"))
-  (check-equal? (parse sentence-parser "goo.gar\n  123\n  456") (stack "foo" "bar" "2-goo" "3-gar+123+456"))
-
-  (check-equal? (parse sentence-parser "foo:")  (failure! parse-incomplete (at (position 1 5))))
-  (check-equal? (parse sentence-parser "foo: ")  (failure! parse-incomplete (at (position 1 6))))
-  (check-equal? (parse sentence-parser "foo(")  (failure! parse-incomplete (at (position 1 5))))
-  (check-equal? (parse sentence-parser "foo.")  (failure! parse-incomplete (at (position 1 5))))
-  (check-equal? (parse sentence-parser "foo,") (failure! parse-incomplete (at (position 1 5))))
-  (check-equal? (parse sentence-parser "foo, ") (failure! parse-complete (at (position 1 5))))
-
-  (check-equal? (parse sentence-parser "Foo") (failure! parse-complete (at (position 1 2))))
-  (check-equal? (parse sentence-parser "fOo") (failure! parse-complete (at (position 1 3))))
-  (check-equal? (parse sentence-parser "foO") (failure! parse-incomplete (at (position 1 4))))
-  (check-equal? (parse sentence-parser "1") (failure! parse-incomplete (at (position 1 2))))
-  (check-equal? (parse sentence-parser "fo1") (failure! parse-incomplete (at (position 1 4))))
-
-  (check-equal? (parse script-parser "") null)
-  (check-equal? (parse script-parser "\n") null)
-
-  (check-equal? (parse script-parser "foo\n") (stack "0-foo"))
-
-  (check-equal? (parse script-parser "foo\nbar\n") (stack "0-foo" "1-bar"))
-  (check-equal? (parse script-parser "foo, bar\n") (stack "0-foo" "1-bar"))
-
-  (check-equal? (parse script-parser "foo\nbar, goo\n") (stack "0-foo" "1-bar" "2-goo"))
-  (check-equal? (parse script-parser "foo, bar\ngoo\n") (stack "0-foo" "1-bar" "2-goo"))
-
-  (check-equal? (parse script-parser "\nfoo, bar\n\ngoo\n\n") (stack "0-foo" "1-bar" "2-goo"))
-
-  (check-equal?
-    (parse script-parser "foo\nbar\ngoo 123\nzar\n  123\n  456\n")
-    (stack "0-foo" "1-bar" "2-goo+123" "3-zar+123+456"))
-
-  (check-equal? (parse script-parser "foo")  (failure! parse-incomplete (at (position 1 4))))
-)
+            (repeat-separated-parser $rhs comma-or-newlines-separator-parser
+              (lambda (($repeated-rhs : V))
+                (env-line-parser $env $repeated-rhs)))
+            (lambda (($parsed-rhs : V))
+              (#%app $end-fn $parsed-rhs))))))))
--- parser/sexp-parser.rkt
+++ parser/sexp-parser.rkt
@@ -4,51 +4,79 @@
   leo/parser/literal-parser
   leo/parser/sentence-parser)
 
-(define sexp-env : (Env (Stackof Sexp) (Stackof Sexp))
+(define sexp-env : (Env (Stackof Sexp))
   (env
+    (lambda (($sexp-stack : (Stackof Sexp)))
+      (parser-map literal-parser
+        (lambda (($literal : Literal))
+          (push $sexp-stack (literal-sexp $literal)))))
     (lambda
       (
         ($sexp-stack : (Stackof Sexp))
         ($symbol : Symbol)
         ($fn : (->
           (Stackof Sexp)
-          (-> (Stackof Sexp) (Parser (Stackof Sexp)))
           (-> (Stackof Sexp) (Stackof Sexp))
           (Parser (Stackof Sexp)))))
       : (Parser (Stackof Sexp))
       (#%app $fn null
-        (lambda (($inner-sexp-stack : (Stackof Sexp))) : (Parser (Stackof Sexp))
-          (plus-sexp-line-parser $inner-sexp-stack))
-        (lambda (($inner-sexp-stack : (Stackof Sexp))) : (Stackof Sexp)
+        (lambda (($rhs : (Stackof Sexp))) : (Stackof Sexp)
           (push $sexp-stack
-            (sexp-stack-sexp $symbol $inner-sexp-stack)))))))
+            (cond
+              ((null? $rhs) $symbol)
+              (else `(,$symbol ,@(reverse $rhs))))))))))
 
-(define (plus-sexp-line-parser ($sexp-stack : (Stackof Sexp))) : (Parser (Stackof Sexp))
-  (parser-or
-    (parser-map literal-parser
-      (lambda (($literal : Literal))
-        (push $sexp-stack (literal-sexp $literal))))
-    (env-sentence-parser sexp-env $sexp-stack)))
-
-(define sexp-stack-parser : (Parser (Stackof Sexp))
-  (env-script-parser sexp-env null
-    (lambda (($sexp-stack : (Stackof Sexp)))
-      (plus-sexp-line-parser $sexp-stack))))
-
-(define (sexp-stack-sexp ($symbol : Symbol) ($sexp-stack : (Stackof Sexp))) : Sexp
-  (cond
-    ((null? $sexp-stack) $symbol)
-    (else `(,$symbol ,@(reverse $sexp-stack)))))
-
-(define (parse-sexp-list ($string : String)) : (U (Listof Sexp) (Failure Any))
-  (bind $result (parse sexp-stack-parser $string)
-    (cond
-      ((failure? $result) $result)
-      (else (reverse $result)))))
+(define (parse-sexp-list ($string : String)) : (U (Stackof Sexp) (Failure Any))
+  (parse
+    (parser-map
+      (env-script-parser sexp-env null)
+      (lambda (($sexp-stack : (Stackof Sexp)))
+        (reverse $sexp-stack)))
+    $string))
 
 (check-equal? (parse-sexp-list "") `())
-(check-equal? (parse-sexp-list "foo\n") `(foo))
-(check-equal? (parse-sexp-list "foo bar\n") `((foo bar)))
-(check-equal? (parse-sexp-list "foo\nbar\n") `(foo bar))
 
-(check-equal? (parse-sexp-list "0, -1, 2, \"foo\"\n") `(0 -1 2 "foo"))
+(check-equal? (parse-sexp-list "foo\n") `(foo))
+(check-equal? (parse-sexp-list "123\n") `(123))
+(check-equal? (parse-sexp-list "\"foo\"\n") `("foo"))
+
+(check-equal? (parse-sexp-list "foo 123\n") `((foo 123)))
+
+(check-equal? (parse-sexp-list "foo\n  123\n") `((foo 123)))
+(check-equal? (parse-sexp-list "foo\n  123\n  456\n") `((foo 123 456)))
+
+(check-equal? (parse-sexp-list "foo: 123\n") `((foo 123)))
+(check-equal? (parse-sexp-list "foo: 123, 456\n") `((foo 123 456)))
+
+(check-equal? (parse-sexp-list "foo()\n") `(foo))
+(check-equal? (parse-sexp-list "foo(123)\n") `((foo 123)))
+(check-equal? (parse-sexp-list "foo(123, 456)\n") `((foo 123 456)))
+
+(check-equal? (parse-sexp-list "foo")  (failure! parse-incomplete (at (position 1 4))))
+(check-equal? (parse-sexp-list "foo:")  (failure! parse-incomplete (at (position 1 5))))
+(check-equal? (parse-sexp-list "foo: ")  (failure! parse-incomplete (at (position 1 6))))
+(check-equal? (parse-sexp-list "foo(")  (failure! parse-incomplete (at (position 1 5))))
+(check-equal? (parse-sexp-list "foo.")  (failure! parse-incomplete (at (position 1 5))))
+(check-equal? (parse-sexp-list "foo,") (failure! parse-incomplete (at (position 1 5))))
+(check-equal? (parse-sexp-list "foo, ") (failure! parse-incomplete (at (position 1 6))))
+
+(check-equal? (parse-sexp-list "Foo") (failure! parse-complete (at (position 1 2))))
+(check-equal? (parse-sexp-list "fOo") (failure! parse-complete (at (position 1 3))))
+(check-equal? (parse-sexp-list "foO") (failure! parse-incomplete (at (position 1 4))))
+(check-equal? (parse-sexp-list "fo1") (failure! parse-incomplete (at (position 1 4))))
+
+(check-equal? (parse-sexp-list "\n") null)
+
+(check-equal? (parse-sexp-list "foo\n") `(foo))
+
+(check-equal? (parse-sexp-list "foo\nbar\n") `(foo bar))
+(check-equal? (parse-sexp-list "foo, bar\n") `(foo bar))
+
+(check-equal? (parse-sexp-list "foo\nbar, goo\n") `(foo bar goo))
+(check-equal? (parse-sexp-list "foo, bar\ngoo\n") `(foo bar goo))
+
+(check-equal? (parse-sexp-list "\nfoo, bar\n\ngoo\n\n") `(foo bar goo))
+
+(check-equal?
+  (parse-sexp-list "foo\nbar\ngoo 123\nzar\n  123\n  456\n")
+  `(foo bar (goo 123) (zar 123 456)))
