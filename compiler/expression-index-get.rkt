240b572e4780bf5ba116e0b5adb5b4b317573003
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-04-11 23:56:46 +0200

    app-if

--- compiler/expression-index-get.rkt
+++ compiler/expression-index-get.rkt
@@ -3,28 +3,32 @@
 (require
   leo/compiler/expression
   leo/compiler/expression-utils
+  leo/compiler/expression-field
   leo/compiler/syntax-utils
   leo/compiler/type
   leo/compiler/type-utils)
 
 (define-type Expression-Index-Get
-  (U Expression 'not-field 'invalid-index))
+  (U
+    Expression
+    'expression-type-not-field
+    'invalid-index))
 
 (define (expression-get-index
   ($expression : Expression)
   ($index : Exact-Nonnegative-Integer))
 : Expression-Index-Get
-  (define $type (expression-type $expression))
+  (define $field (expression-field $expression))
   (cond
-    ((field? $type)
+    ((field? $field)
       (define $syntax-option (expression-syntax-option $expression))
-      (define $structure (field-structure $type))
+      (define $structure (field-structure $field))
       (cond
         ((< $index (length $structure))
           (syntax-option-structure-ref $syntax-option $structure $index))
         (else
-          `invalid-index)))
-    (else `not-field)))
+          'invalid-index)))
+    (else $field)))
 
 (check-equal?
   (app-if expression? expression-sexp
@@ -49,4 +53,4 @@
     (expression-get-index
       (expression syntax-a (racket))
       0))
-  `not-field)
+  `expression-type-not-field)
