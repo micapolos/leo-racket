c447e3fd6dc0eda4755565f697eea0b873e2883f
Michał Pociecha-Łoś <micapolos@gmail.com>
2023-04-15 16:11:00 +0200

    index-syntax-structure-select-expression

--- compiler/expression-index-select.rkt
+++
@@ -1,4 +0,0 @@
-#lang leo/typed
-
-(define (resolve-expression-choice-cast ($expression : Expression) ($choice : Choice)) : (Option Expression)
-  (structure-dynamic-index (choice-structure)
--- compiler/select-expression-utils.rkt
+++ compiler/select-expression-utils.rkt
@@ -10,28 +10,29 @@
 (define (index-syntax-structure-select-expression 
   ($index : Exact-Nonnegative-Integer)
   ($syntax-option : (Option Syntax))
-  ($structure : Structure)) : Expression
+  ($structure : Structure))
+: Expression
   (define $size (length $structure))
   (expression
-    (and $syntax-option
-      (make-syntax
-        (case $size
-          ((0) `(error))
-          ((1) $syntax-option)
-          (else
-            (define $selector
-              (if (= $size 2)
-                (= $index 0)
-                $index))
-            (if (structure-dynamic? $structure)
-              `(cons ,$selector ,$syntax-option)
-              $selector)))))
+    (make-syntax
+      (case $size
+        ((0) `(error))
+        ((1) $syntax-option)
+        (else
+          (define $selector
+            (if (= $size 2)
+              (= $index 0)
+              $index))
+          (if (structure-dynamic? $structure)
+            `(cons ,$selector ,(or $syntax-option #f))
+            $selector))))
     (choice $structure)))
 
 (check-equal?
-  (expression-sexp-type
+  (expression-sexp
     (index-syntax-structure-select-expression 0 #`stx null-structure))
-  (pair `(error) (choice null-structure)))
+  (expression-sexp
+    (expression #`(error) (choice!))))
 
 (check-equal?
   (expression-sexp
@@ -41,51 +42,79 @@
     (expression #f (choice! static-type-a))))
 
 (check-equal?
-  (expression-sexp-type
+  (expression-sexp
     (index-syntax-structure-select-expression 0 #`stx
       (structure dynamic-type-a)))
-  (pair `stx (choice (structure dynamic-type-a))))
+  (expression-sexp
+    (expression #`stx (choice! dynamic-type-a))))
 
 (check-equal?
-  (expression-sexp-type
+  (expression-sexp
     (index-syntax-structure-select-expression 0 #f
       (structure static-type-a static-type-b)))
-  (pair #f (choice (structure static-type-a static-type-b))))
+  (expression-sexp
+    (expression #`#t (choice! static-type-a static-type-b))))
 
 (check-equal?
-  (expression-sexp-type
+  (expression-sexp
     (index-syntax-structure-select-expression 1 #f
       (structure static-type-a static-type-b)))
-  (pair #f (choice (structure static-type-a static-type-b))))
+  (expression-sexp
+    (expression #`#f (choice! static-type-a static-type-b))))
 
 (check-equal?
-  (expression-sexp-type
+  (expression-sexp
     (index-syntax-structure-select-expression 0 #`stx
       (structure dynamic-type-a static-type-b)))
-  (pair `(cons #t stx) (choice (structure dynamic-type-a static-type-b))))
+  (expression-sexp
+    (expression
+      #`(cons #t stx)
+      (choice! dynamic-type-a static-type-b))))
 
 (check-equal?
-  (expression-sexp-type
+  (expression-sexp
     (index-syntax-structure-select-expression 1 #`stx
       (structure dynamic-type-a static-type-b)))
-  (pair `(cons #f stx) (choice (structure dynamic-type-a static-type-b))))
+  (expression-sexp
+    (expression
+      #`(cons #f stx)
+      (choice! dynamic-type-a static-type-b))))
 
 (check-equal?
-  (expression-sexp-type
+  (expression-sexp
     (index-syntax-structure-select-expression 
       0
       #f
       (structure static-type-a static-type-b static-type-c)))
-  (pair 
-    #f
-    (choice (structure static-type-a static-type-b static-type-c))))
+  (expression-sexp
+    (expression
+      #`0
+      (choice! static-type-a static-type-b static-type-c))))
 
 (check-equal?
-  (expression-sexp-type
+  (expression-sexp
     (index-syntax-structure-select-expression 
       0
       #`stx
       (structure dynamic-type-a static-type-b dynamic-type-c)))
-  (pair 
-    `(cons 0 stx) 
-    (choice (structure dynamic-type-a static-type-b dynamic-type-c))))
+  (expression-sexp
+    (expression
+      #`(cons 0 stx)
+      (choice! dynamic-type-a static-type-b dynamic-type-c))))
+
+; ---------------------------------------------------------
+
+(define (expression-choice-cast ($expression : Expression) ($choice : Choice)) : (Option Expression)
+  (define $structure (choice-type-stack $choice))
+  (option-bind (structure-index-matching-type $structure (expression-type $expression)) $index
+  (index-syntax-structure-select-expression
+    $index
+    (expression-syntax-option $expression)
+    $structure)))
+
+(bind $choice (choice! (field! `foo) (field! `bar) (field! `goo))
+  (check-equal?
+    (option-app expression-sexp
+      (expression-choice-cast (field-expression! foo) $choice))
+    (expression-sexp
+      (expression #`2 $choice))))
